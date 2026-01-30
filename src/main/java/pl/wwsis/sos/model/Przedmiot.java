db08254c4c05e49585b6f40cb1586687373de6dd
MateuszGalusza <mateusz_galusza@wp.pl>
2026-01-30 12:35:05 +0100

    feat(entity): refine Przedmiot JPA mapping

--- src/main/java/pl/wwsis/sos/model/Przedmiot.java
+++ src/main/java/pl/wwsis/sos/model/Przedmiot.java
@@ -1,65 +1,16 @@
-package pl.wwsis.sos.model;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "id_przedmiotu", nullable = false, updatable = false)
+private Integer id;
 
-import javax.persistence.*;
+@Column(name = "kod_przedmiotu", length = 20, nullable = false, unique = true)
+private String kodPrzedmiotu;
 
-@Entity
-@Table(name = "Przedmiot")
-public class Przedmiot {
+@Column(name = "nazwa", length = 100, nullable = false)
+private String nazwa;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_przedmiotu")
-    private Integer id;
+@Column(name = "opis", columnDefinition = "TEXT")
+private String opis;
 
-    @Column(name = "kod_przedmiotu", length = 20)
-    private String kodPrzedmiotu;
-
-    @Column(name = "nazwa", length = 100)
-    private String nazwa;
-
-    @Column(name = "opis", columnDefinition = "TEXT")
-    private String opis;
-
-    @Column(name = "ects")
-    private Integer ects;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getKodPrzedmiotu() {
-        return kodPrzedmiotu;
-    }
-
-    public void setKodPrzedmiotu(String kodPrzedmiotu) {
-        this.kodPrzedmiotu = kodPrzedmiotu;
-    }
-
-    public String getNazwa() {
-        return nazwa;
-    }
-
-    public void setNazwa(String nazwa) {
-        this.nazwa = nazwa;
-    }
-
-    public String getOpis() {
-        return opis;
-    }
-
-    public void setOpis(String opis) {
-        this.opis = opis;
-    }
-
-    public Integer getEcts() {
-        return ects;
-    }
-
-    public void setEcts(Integer ects) {
-        this.ects = ects;
-    }
-}
+@Column(name = "ects", nullable = false)
+private Integer ects;
