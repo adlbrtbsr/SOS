e645f0bde5a3938a896cfa84ab6f784f83c25a27
MateuszGalusza <mateusz_galusza@wp.pl>
2026-01-30 12:45:03 +0100

    feat(entity): refine Ksiazka JPA mapping (basic)

--- src/main/java/pl/wwsis/sos/model/Ksiazka.java
+++ src/main/java/pl/wwsis/sos/model/Ksiazka.java
@@ -1,54 +1,13 @@
-package pl.wwsis.sos.model;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "id_ksiazki", nullable = false, updatable = false)
+private Integer id;
 
-import javax.persistence.*;
+@Column(name = "tytul", length = 200, nullable = false)
+private String tytul;
 
-@Entity
-@Table(name = "Ksiazka")
-public class Ksiazka {
+@Column(name = "autor", length = 200, nullable = false)
+private String autor;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_ksiazki")
-    private Integer id;
-
-    @Column(name = "tytul", length = 200)
-    private String tytul;
-
-    @Column(name = "autor", length = 200)
-    private String autor;
-
-    @Column(name = "isbn", length = 20)
-    private String isbn;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTytul() {
-        return tytul;
-    }
-
-    public void setTytul(String tytul) {
-        this.tytul = tytul;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-}
+@Column(name = "isbn", length = 20, nullable = false)
+private String isbn;
