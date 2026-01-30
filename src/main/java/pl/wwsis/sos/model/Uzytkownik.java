b351c07b6a14817c010f98cbf70d4a176d131236
MateuszGalusza <mateusz_galusza@wp.pl>
2026-01-30 12:31:45 +0100

    feat(entity): add constraints to Uzytkownik columns

--- src/main/java/pl/wwsis/sos/model/Uzytkownik.java
+++ src/main/java/pl/wwsis/sos/model/Uzytkownik.java
@@ -1,78 +1,20 @@
-package pl.wwsis.sos.model;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "id_uzytkownika", nullable = false, updatable = false)
+private Integer id;
 
-import javax.persistence.*;
-import java.util.Date;
+@Column(name = "login", length = 50, nullable = false, unique = true)
+private String login;
 
-@Entity
-@Table(name = "Uzytkownik")
-public class Uzytkownik {
+@Column(name = "email", length = 100, nullable = false, unique = true)
+private String email;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_uzytkownika")
-    private Integer id;
+@Column(name = "haslo_hash", length = 255, nullable = false)
+private String hasloHash;
 
-    @Column(name = "login", length = 50)
-    private String login;
+@Column(name = "status_konta", length = 20, nullable = false)
+private String statusKonta;
 
-    @Column(name = "email", length = 100)
-    private String email;
-
-    @Column(name = "haslo_hash", length = 255)
-    private String hasloHash;
-
-    @Column(name = "status_konta", length = 20)
-    private String statusKonta;
-
-    @Temporal(TemporalType.DATE)
-    @Column(name = "data_rejestracji")
-    private Date dataRejestracji;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getHasloHash() {
-        return hasloHash;
-    }
-
-    public void setHasloHash(String hasloHash) {
-        this.hasloHash = hasloHash;
-    }
-
-    public String getStatusKonta() {
-        return statusKonta;
-    }
-
-    public void setStatusKonta(String statusKonta) {
-        this.statusKonta = statusKonta;
-    }
-
-    public Date getDataRejestracji() {
-        return dataRejestracji;
-    }
-
-    public void setDataRejestracji(Date dataRejestracji) {
-        this.dataRejestracji = dataRejestracji;
-    }
-}
+@Temporal(TemporalType.DATE)
+@Column(name = "data_rejestracji", nullable = false)
+private Date dataRejestracji;
