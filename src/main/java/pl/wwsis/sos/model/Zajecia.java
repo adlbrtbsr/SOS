16d9982efe198aa5ad17e4fb2009321c53184ac1
MateuszGalusza <mateusz_galusza@wp.pl>
2026-01-30 12:39:04 +0100

    feat(entity): refine Zajecia JPA mapping

--- src/main/java/pl/wwsis/sos/model/Zajecia.java
+++ src/main/java/pl/wwsis/sos/model/Zajecia.java
@@ -1,92 +1,26 @@
-package pl.wwsis.sos.model;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "id_zajec", nullable = false, updatable = false)
+private Integer id;
 
-import javax.persistence.*;
-import java.util.Date;
+@ManyToOne
+@JoinColumn(name = "id_grupy", nullable = false)
+private GrupaPrzedmiotowa grupa;
 
-@Entity
-@Table(name = "Zajecia")
-public class Zajecia {
+@ManyToOne
+@JoinColumn(name = "id_semestru", nullable = false)
+private Semestr semestr;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_zajec")
-    private Integer id;
+@Column(name = "dzien_tygodnia", nullable = false)
+private Short dzienTygodnia;
 
-    @ManyToOne
-    @JoinColumn(name = "id_grupy")
-    private GrupaPrzedmiotowa grupa;
+@Temporal(TemporalType.TIME)
+@Column(name = "godzina_od", nullable = false)
+private Date godzinaOd;
 
-    @ManyToOne
-    @JoinColumn(name = "id_semestru")
-    private Semestr semestr;
+@Temporal(TemporalType.TIME)
+@Column(name = "godzina_do", nullable = false)
+private Date godzinaDo;
 
-    @Column(name = "dzien_tygodnia")
-    private Short dzienTygodnia;
-
-    @Temporal(TemporalType.TIME)
-    @Column(name = "godzina_od")
-    private Date godzinaOd;
-
-    @Temporal(TemporalType.TIME)
-    @Column(name = "godzina_do")
-    private Date godzinaDo;
-
-    @Column(name = "sala", length = 20)
-    private String sala;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public GrupaPrzedmiotowa getGrupa() {
-        return grupa;
-    }
-
-    public void setGrupa(GrupaPrzedmiotowa grupa) {
-        this.grupa = grupa;
-    }
-
-    public Semestr getSemestr() {
-        return semestr;
-    }
-
-    public void setSemestr(Semestr semestr) {
-        this.semestr = semestr;
-    }
-
-    public Short getDzienTygodnia() {
-        return dzienTygodnia;
-    }
-
-    public void setDzienTygodnia(Short dzienTygodnia) {
-        this.dzienTygodnia = dzienTygodnia;
-    }
-
-    public Date getGodzinaOd() {
-        return godzinaOd;
-    }
-
-    public void setGodzinaOd(Date godzinaOd) {
-        this.godzinaOd = godzinaOd;
-    }
-
-    public Date getGodzinaDo() {
-        return godzinaDo;
-    }
-
-    public void setGodzinaDo(Date godzinaDo) {
-        this.godzinaDo = godzinaDo;
-    }
-
-    public String getSala() {
-        return sala;
-    }
-
-    public void setSala(String sala) {
-        this.sala = sala;
-    }
-}
+@Column(name = "sala", length = 20, nullable = false)
+private String sala;
